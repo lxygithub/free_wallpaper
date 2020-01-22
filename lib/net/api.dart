8ccf0d37edea6e0dd7f2f5cddaa5eb6cef5f1650
lxygithub <594328762@qq.com>
2020-01-23 00:04:41 +0800

    首页

---
+++ lib/net/api.dart
@@ -0,0 +1,10 @@
+/*
+  description:
+  author:59432
+  create_time:2020/1/22 23:34
+*/
+class Api {
+
+//https://bird.ioliu.cn/v2?url=http://wallpaper.apc.360.cn/index.php?c=WallPaper&start=25&count=12&from=360chrome&a=getAppsByOrder&order=create_time
+  static const String HOME_DATA_FLOW = "https://bird.ioliu.cn/v2?url=http://wallpaper.apc.360.cn/index.php?c=WallPaper&start=%d&count=12&from=360chrome&a=getAppsByOrder&order=create_time";
+}
--- lib/pages/page_home.dart
+++ lib/pages/page_home.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:free_wallpaper/net/api.dart';
 
 /*
   description:
@@ -15,9 +17,25 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home"),
-      ),
+      body: GridView.count(
+      // Create a grid with 2 columns. If you change the scrollDirection to
+      // horizontal, this produces 2 rows.
+      crossAxisCount: 2,
+      // Generate 100 widgets that display their index in the List.
+      children: List.generate(100, (index) {
+        return Center(
+          child: Text(
+            'Item $index',
+            style: Theme.of(context).textTheme.headline,
+          ),
+        );
+      }),
+    ),
     );
   }
+}
+
+_requestDataFlow(int startPage){
+  Dio dio = new Dio();
+  dio.get(Api.HOME_DATA_FLOW)
 }
